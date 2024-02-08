3dd55a73952de2ae4f3329c8a98d1eeb235275b4
Gabriel Souza <anderson.rosario@ufv.br>
2024-02-08 10:17:47 -0300

    View que direciona os cards à página de exibição

--- site_da_Julia/cards/views.py
+++ site_da_Julia/cards/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Card
 
-# Create your views here.
+def cards(request):
+    if request.method == "GET":
+        dados = Card.objects.order_by('-id').filter(publicada=True).all
+        return render(request, 'cards/cards.html', {'cards': dados})
+    else:
+        nome_do_card = request.POST.get('nome_do_card')
+        card = get_object_or_404(Card, nome_do_card=nome_do_card)
+        resposta = request.POST.get('resposta') 
+
+        if resposta == card.resposta_correta:
+            return HttpResponse(' é a resposta correta!')
+        else:
+            return HttpResponse(' é a resposta errada!')
+    # Path: site_da_julia/cards/urls.py
