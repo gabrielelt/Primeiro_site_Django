246a6331a4d33e3b7b5aed06eb44cf9783950162
Gabriel Souza <anderson.rosario@ufv.br>
2024-02-08 10:03:16 -0300

    Adicionar Card ao sistema de admin

--- site_da_Julia/cards/admin.py
+++ site_da_Julia/cards/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from cards.models import Card
 
-# Register your models here.
+class listando_cards(admin.ModelAdmin):
+    list_display = ('id', 'nome_do_card', 'resposta_correta', 'publicada')
+    list_display_links = ('id', 'nome_do_card')
+    search_fields = ('nome_do_card',)
+    list_filter = ('classe_do_card',)
+    list_editable = ('publicada',)
+    list_per_page = 10
+
+admin.site.register(Card, listando_cards)
