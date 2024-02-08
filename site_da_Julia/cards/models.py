1751ff5e4cd0b35735d4d53b5f33b3d99bef1c76
Gabriel Souza <anderson.rosario@ufv.br>
2024-02-08 10:11:53 -0300

    Criação da tabela de Cards no banco de dados

--- site_da_Julia/cards/models.py
+++ site_da_Julia/cards/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class Card(models.Model):
+    CLASSES_CARDS = [
+        ('', 'Selecione a classe do card'),
+        ('Frutas', 'Frutas'),
+        ('Animais', 'Animais'),
+        ('Objetos', 'Objetos'),
+        ('Pessoas', 'Pessoas'),
+        ('Veículos', 'Veículos'), 
+    ]
+
+    nome_do_card = models.CharField(max_length=100, null=False, blank=False)
+    classe_do_card = models.CharField(max_length=100, default='', choices = CLASSES_CARDS)
+    publicada = models.BooleanField(default=False)
+    alternativa_um = models.CharField(max_length=100, null=False, blank=False)
+    alternativa_dois = models.CharField(max_length=100, null=False, blank=False)
+    alternativa_tres = models.CharField(max_length=100, null=False, blank=False)
+    alternativa_quatro = models.CharField(max_length=100, null=False, blank=False)
+    alternativa_cinco = models.CharField(max_length=100, null=False, blank=False)
+
+    resposta_correta = models.CharField(max_length=100, null= False, blank=False)
+ 
+    foto_card = models.ImageField(upload_to='cards_fotos/%Y/%m/%d/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.nome_do_card
