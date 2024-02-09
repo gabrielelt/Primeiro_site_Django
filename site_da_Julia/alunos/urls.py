02067b8a400b3f45e4158b75dae4c2d34e4f1faf
Gabriel Souza <anderson.rosario@ufv.br>
2024-02-09 00:34:46 -0300

    Redirecionar o usuário para a página de destino, após autenticação, se necessária

--- site_da_Julia/alunos/urls.py
+++ site_da_Julia/alunos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
-    path('login/', views.login, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('home/', views.home, name='home'),
 ]
