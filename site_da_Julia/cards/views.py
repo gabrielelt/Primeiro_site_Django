9a00919f2ba080fb1f4d42e969567c7cf32b56a0
Gabriel Souza <anderson.rosario@ufv.br>
2024-02-09 00:32:00 -0300

    Exigir autenticação para acessar os cards

--- site_da_Julia/cards/views.py
+++ site_da_Julia/cards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Card
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def cards(request):
     if request.method == "GET":
         dados = Card.objects.order_by('-id').filter(publicada=True).all
