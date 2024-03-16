c98e0979251df5ba9396d8f6f001172906951cf6
Gabriel Souza <anderson.rosario@ufv.br>
2024-03-15 22:23:43 -0300

    requirements

--- site_da_Julia/alunos/models.py
+++ site_da_Julia/alunos/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class Aluno_Profile(AbstractUser):
-    matricula = models.CharField(max_length=10, unique=True)
-    curso = models.CharField(max_length=100)
+class Aluno_Profile( AbstractUser):
 
     def __str__(self):
         return self.username
